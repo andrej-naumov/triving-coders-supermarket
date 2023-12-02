73c7e88ee5b6dd1d324893f25d02e6363c1ffcd3
Andrej Naumov <andrejs@list.ru>
2023-12-02 14:41:22 +0100

    added Message class for Response JSON

---
+++ src/main/java/org/thriving/coders/supermarket/util/Message.java
@@ -0,0 +1,11 @@
+package org.thriving.coders.supermarket.util;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Message {
+
+    private MessageLevel messagesLevel;
+    private  String message;
+}
+
---
+++ src/main/java/org/thriving/coders/supermarket/util/MessageLevel.java
@@ -0,0 +1,8 @@
+package org.thriving.coders.supermarket.util;
+
+public enum MessageLevel {
+    INFO,
+    SUCCESS,
+    WARNING,
+    ERROR
+}
