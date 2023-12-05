4c0763edbba752d0e775c77dc91b900648614cbc
Andrej Naumov <andrejs@list.ru>
2023-12-05 17:35:25 +0100

    refactoring - add headers for CORS in private method

--- src/main/java/org/thriving/coders/Main.java
+++ src/main/java/org/thriving/coders/Main.java
@@ -6,12 +6,31 @@ import org.hibernate.SessionFactory;
 import org.thriving.coders.supermarket.personnel.controller.EmployeeController;
 import org.thriving.coders.supermarket.personnel.services.EmployeeService;
 import org.thriving.coders.supermarket.util.HibernateUtil;
+import spark.Request;
+import spark.Response;
+
+import static spark.route.HttpMethod.options;
+import static spark.Spark.*;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) {
         // server startup information
         log.info("server goes live...");
+
+        // enable CORS for all req from http://localhost:4200
+        options("/*", (request, response) -> {
+            addEnableCORS_Headers(request, response);
+
+            return "OK";
+        });
+
+        // enable CORS for all req from http://localhost:4200
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "http://localhost:4200");
+            response.header("Access-Control-Allow-Credentials", "true");
+        });
+        //~
 
         final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         final ObjectMapper objectMapper = new ObjectMapper();
@@ -21,5 +40,17 @@ public class Main {
 
         // server startup information
         log.info("...server is running");
+    }
+
+    private static void addEnableCORS_Headers(Request request, Response response) {
+        String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+        if (accessControlRequestHeaders != null) {
+            response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+        }
+
+        String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+        if (accessControlRequestMethod != null) {
+            response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+        }
     }
 }
