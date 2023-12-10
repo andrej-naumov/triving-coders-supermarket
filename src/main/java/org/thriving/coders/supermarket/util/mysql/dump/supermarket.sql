615fb8527939cde947b4321cdd023d7f0eca3b1d
andrej-naumov <andrejs@list.ru>
2023-12-10 23:10:52 +0100

    at employees_schedules table - workStart & workEnd - changed from date to datetime

--- src/main/java/org/thriving/coders/supermarket/util/mysql/dump/supermarket.sql
+++ src/main/java/org/thriving/coders/supermarket/util/mysql/dump/supermarket.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2023 at 09:12 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Erstellungszeit: 10. Dez 2023 um 23:03
+-- Server-Version: 10.4.32-MariaDB
+-- PHP-Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,124 +18,215 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `supermarket`
+-- Datenbank: `supermarket`
 --
+
+
+
+--
+-- Tabellenstruktur für Tabelle `employees_assessments`
+--
+
+DROP TABLE IF EXISTS `employees_assessments`;
+CREATE TABLE `employees_assessments` (
+  `assessmentId` int(11) NOT NULL,
+  `employeeId` int(11) NOT NULL,
+  `assessmentDate` date NOT NULL,
+  `performanceRating` int(11) NOT NULL,
+  `salesAnalysis` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- TRUNCATE Tabelle vor dem Einfügen `employees_assessments`
+--
+
+TRUNCATE TABLE `employees_assessments`;
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `employees_schedules`
+--
+
+DROP TABLE IF EXISTS `employees_schedules`;
+CREATE TABLE `employees_schedules` (
+  `scheduleId` int(11) NOT NULL,
+  `employeeId` int(11) NOT NULL,
+  `shiftDate` date NOT NULL,
+  `shiftType` int(11) NOT NULL,
+  `workStart` datetime NOT NULL,
+  `workEnd` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- TRUNCATE Tabelle vor dem Einfügen `employees_schedules`
+--
+
+TRUNCATE TABLE `employees_schedules`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employees`
+-- Tabellenstruktur für Tabelle `employees_sicks`
+--
+
+DROP TABLE IF EXISTS `employees_sicks`;
+CREATE TABLE `employees_sicks` (
+  `sickLeaveId` int(11) NOT NULL,
+  `employeeId` int(11) NOT NULL,
+  `startDate` date NOT NULL,
+  `endDate` date NOT NULL,
+  `medicalCertificate` mediumblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- TRUNCATE Tabelle vor dem Einfügen `employees_sicks`
+--
+
+TRUNCATE TABLE `employees_sicks`;
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `employees_vacations`
+--
+
+DROP TABLE IF EXISTS `employees_vacations`;
+CREATE TABLE `employees_vacations` (
+  `vacationId` int(11) NOT NULL,
+  `employeeId` int(11) NOT NULL,
+  `startDate` date NOT NULL,
+  `endDate` date NOT NULL,
+  `reason` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- TRUNCATE Tabelle vor dem Einfügen `employees_vacations`
+--
+
+TRUNCATE TABLE `employees_vacations`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `employees`
 --
 
 DROP TABLE IF EXISTS `employees`;
-CREATE TABLE IF NOT EXISTS `employees` (
-  `employeeId` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `employees` (
+  `employeeId` int(11) NOT NULL,
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
   `position` varchar(255) NOT NULL,
   `department` int(11) NOT NULL,
   `contactInfo` varchar(255) NOT NULL,
   `hourlyRate` int(11) NOT NULL,
-  `commentary` varchar(255) NOT NULL,
-  PRIMARY KEY (`employeeId`),
-  KEY `employeeId` (`employeeId`)
-) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Thriving Coders - Supermarkt Project';
+  `commentary` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Thriving Coders - Supermarkt Project';
 
+--
+-- TRUNCATE Tabelle vor dem Einfügen `employees`
+--
 
+TRUNCATE TABLE `employees`;
 -- --------------------------------------------------------
 
---
--- Table structure for table `employees_assessments`
---
-
-DROP TABLE IF EXISTS `employees_assessments`;
-CREATE TABLE IF NOT EXISTS `employees_assessments` (
-  `assessmentId` int(11) NOT NULL AUTO_INCREMENT,
-  `employeeId` int(11) NOT NULL,
-  `assessmentDate` date NOT NULL,
-  `performanceRating` int(11) NOT NULL,
-  `salesAnalysis` int(11) NOT NULL,
-  PRIMARY KEY (`assessmentId`),
-  KEY `employeeId` (`employeeId`)
-) ENGINE=InnoDB AUTO_INCREMENT=131 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
--- --------------------------------------------------------
 
 --
--- Table structure for table `employees_schedules`
---
-
-DROP TABLE IF EXISTS `employees_schedules`;
-CREATE TABLE IF NOT EXISTS `employees_schedules` (
-  `scheduleId` int(11) NOT NULL AUTO_INCREMENT,
-  `employeeId` int(11) NOT NULL,
-  `shiftDate` date NOT NULL,
-  `shiftType` int(11) NOT NULL,
-  `workStart` date NOT NULL,
-  `workEnd` date NOT NULL,
-  PRIMARY KEY (`scheduleId`),
-  KEY `employeeId` (`employeeId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employees_sicks`
---
-
-DROP TABLE IF EXISTS `employees_sicks`;
-CREATE TABLE IF NOT EXISTS `employees_sicks` (
-  `sickLeaveId` int(11) NOT NULL AUTO_INCREMENT,
-  `employeeId` int(11) NOT NULL,
-  `startDate` date NOT NULL,
-  `endDate` date NOT NULL,
-  `medicalCertificate` mediumblob NOT NULL,
-  PRIMARY KEY (`sickLeaveId`),
-  KEY `employeeId` (`employeeId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employees_vacations`
---
-
-DROP TABLE IF EXISTS `employees_vacations`;
-CREATE TABLE IF NOT EXISTS `employees_vacations` (
-  `vacationId` int(11) NOT NULL AUTO_INCREMENT,
-  `employeeId` int(11) NOT NULL,
-  `startDate` date NOT NULL,
-  `endDate` date NOT NULL,
-  `reason` text NOT NULL,
-  PRIMARY KEY (`vacationId`),
-  KEY `employeeId` (`employeeId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Constraints for dumped tables
+-- Indizes der exportierten Tabellen
 --
 
 --
--- Constraints for table `employees_assessments`
+-- Indizes für die Tabelle `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`employeeId`),
+  ADD KEY `employeeId` (`employeeId`);
+
+--
+-- Indizes für die Tabelle `employees_assessments`
+--
+ALTER TABLE `employees_assessments`
+  ADD PRIMARY KEY (`assessmentId`),
+  ADD KEY `employeeId` (`employeeId`);
+
+--
+-- Indizes für die Tabelle `employees_schedules`
+--
+ALTER TABLE `employees_schedules`
+  ADD PRIMARY KEY (`scheduleId`),
+  ADD KEY `employeeId` (`employeeId`);
+
+--
+-- Indizes für die Tabelle `employees_sicks`
+--
+ALTER TABLE `employees_sicks`
+  ADD PRIMARY KEY (`sickLeaveId`),
+  ADD KEY `employeeId` (`employeeId`);
+
+--
+-- Indizes für die Tabelle `employees_vacations`
+--
+ALTER TABLE `employees_vacations`
+  ADD PRIMARY KEY (`vacationId`),
+  ADD KEY `employeeId` (`employeeId`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `employeeId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=126;
+
+--
+-- AUTO_INCREMENT für Tabelle `employees_assessments`
+--
+ALTER TABLE `employees_assessments`
+  MODIFY `assessmentId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=159;
+
+--
+-- AUTO_INCREMENT für Tabelle `employees_schedules`
+--
+ALTER TABLE `employees_schedules`
+  MODIFY `scheduleId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=421;
+
+--
+-- AUTO_INCREMENT für Tabelle `employees_sicks`
+--
+ALTER TABLE `employees_sicks`
+  MODIFY `sickLeaveId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `employees_vacations`
+--
+ALTER TABLE `employees_vacations`
+  MODIFY `vacationId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `employees_assessments`
 --
 ALTER TABLE `employees_assessments`
   ADD CONSTRAINT `employees_assessments_ibfk_1` FOREIGN KEY (`employeeId`) REFERENCES `employees` (`employeeId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `employees_schedules`
+-- Constraints der Tabelle `employees_schedules`
 --
 ALTER TABLE `employees_schedules`
   ADD CONSTRAINT `employees_schedules_ibfk_1` FOREIGN KEY (`employeeId`) REFERENCES `employees` (`employeeId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `employees_sicks`
+-- Constraints der Tabelle `employees_sicks`
 --
 ALTER TABLE `employees_sicks`
   ADD CONSTRAINT `employees_sicks_ibfk_1` FOREIGN KEY (`employeeId`) REFERENCES `employees` (`employeeId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `employees_vacations`
+-- Constraints der Tabelle `employees_vacations`
 --
 ALTER TABLE `employees_vacations`
   ADD CONSTRAINT `employees_vacations_ibfk_1` FOREIGN KEY (`vacationId`) REFERENCES `employees` (`employeeId`) ON DELETE CASCADE ON UPDATE CASCADE;
